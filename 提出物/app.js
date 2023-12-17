930eb615bb8646cf0be31363e4945f92d4c16f08
sho828 <153625169+sho828@users.noreply.github.com>
2023-12-17 21:37:57 +0900

    課題を提出するため

---
+++ 提出物/app.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 50; i++) {
+  // console.log(i)
+  if (i == 10) {
+    console.log("今10回ループしました。") 
+  } else if (i == 20) {
+    console.log("今20回ループしました。")
+  } else if (i == 30) {
+    console.log("今30回ループしました。")
+  } else if (i == 40) {
+    console.log("今40回ループしました。")
+  } else if (i == 50) {
+    console.log("今50回ループしました。")
+    alert("50回カウントが終わりました。")
+  }
+
+  if (i % 4 == 0) {
+    console.log("4で割れる数です。" + i)
+  }
+}
